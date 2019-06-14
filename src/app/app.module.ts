b12c184267371fe7eb478b90ee2dd6c89289e7fd
iwaniukmarcin <this.marcin>
2019-06-14 21:45:22 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductComponent } from './components/product/product.component';
+
+import { ProductService } from './services/product.service';
+
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    routing
   ],
-  providers: [],
+  providers: [
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+import { Product } from '../entities/product.entity';
+
+@Injectable()
+export class ProductService {
+
+    private products: Product[];
+
+    constructor() {
+        this.products = [
+            { id: 'p01', name: 'Shoe Nike', price: 190, photo: 'nike.png' },
+            { id: 'p02', name: 'Shoe Asics', price: 180, photo: 'asics.png' },
+            { id: 'p03', name: 'Shoe New Balance', price: 170, photo: 'nb.png' }
+        ];
+    }
+
+    findAll(): Product[] {
+        return this.products;
+    }
+
+    find(id: string): Product {
+        return this.products[this.getSelectedIndex(id)];
+    }
+
+    private getSelectedIndex(id: string) {
+        for (var i = 0; i < this.products.length; i++) {
+            if (this.products[i].id == id) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
